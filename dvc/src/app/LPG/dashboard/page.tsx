7140d0734407314744c7913fa125d7e972f38fc3
BeastMaster26 <ibnu.anandiartha@gmail.com>
2026-02-20 09:03:47 +0700

    some changes

--- dvc/src/app/LPG/dashboard/page.tsx
+++ dvc/src/app/LPG/dashboard/page.tsx
@@ -30,75 +30,11 @@ import { Capsule } from "@/components/Capsule";
 
 
 
-const lineData = [
-{ day: "06 Jan", value: 800000 },
-{ day: "07 Jan", value: 0 },
-{ day: "08 Jan", value: 400000 },
-{ day: "09 Jan", value: 1200000 },
-{ day: "10 Jan", value: 1600000 },
-{ day: "11 Jan", value: 0 },
-{ day: "12 Jan", value: 0 },
-]
-
-
-const tabungData = [
-{ day: "06 Jan", value: 50 },
-{ day: "07 Jan", value: 0 },
-{ day: "08 Jan", value: 25 },
-{ day: "09 Jan", value: 75 },
-{ day: "10 Jan", value: 100 },
-{ day: "11 Jan", value: 0 },
-{ day: "12 Jan", value: 0 },
-]
 export default function DashboardPage(){
     const [open, setOpen] = useState(false);
-    const router = useRouter();
-    const [currentDate, setCurrentDate] = useState(new Date());
-    const daysData = [
-    {
-      day: "Senin",
-      date: "2026-02-02", 
-      count: 200,
-      label: "Tabung",
-      status: "success",
-    },
-    {
-      day: "Selasa",
-      date: "2026-02-03",
-      count: 156,
-      label: "Tabung",
-      status: "success",
-    },
-    {
-      day: "Rabu",
-      date: "2026-02-04",
-      count: 80,
-      label: "Tabung",
-      status: "success",
-    },
-    {
-      day: "Kamis",
-      date: "2026-02-05", 
-      count: 0,
-      label: "Tabung",
-      status: "error",
-    },
-    {
-      day: "Jumat",
-      date: "2026-02-06",
-      count: 178,
-      label: "Tabung",
-      status: "success",
-    },
-  ];
+   
 
-   const isToday = (dateString: string) => {
-    const itemDate = new Date(dateString);
-    return (
-    itemDate.getDate() === currentDate.getDate() && itemDate.getMonth() === currentDate.getMonth() 
-    && itemDate.getFullYear() === currentDate.getFullYear() 
-    )
-   }
+   
 
     return (
         
