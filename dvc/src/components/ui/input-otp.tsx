9151601dd08a3a3dd2fa6426b3c5a21ea73966e8
BeastMaster26 <ibnu.anandiartha@gmail.com>
2026-07-16 09:34:32 +0700

    feat: small changes

--- dvc/src/components/ui/input-otp.tsx
+++ dvc/src/components/ui/input-otp.tsx
@@ -12,7 +12,7 @@ interface OTPContextValue {
 
 const OTPContext = React.createContext<OTPContextValue | null>(null);
 
-export interface InputOTPProps extends Omit<React.HTMLAttributes<HTMLDivElement>, "onChange"> {
+export interface InputOTPProps extends React.ComponentPropsWithoutRef<"div"> {
   maxLength?: number;
   onChange?: (value: string) => void;
   children: React.ReactNode;
